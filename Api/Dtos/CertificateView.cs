633c427586e6c64e6fbc38c511f0b1ddfc41387b
FraChr <empynia@gmail.com>
2026-07-02 18:44:32 +0200

    certificate dto for frontend

---
+++ Api/Dtos/CertificateView.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Dtos;
+
+public class CertificateView
+{
+    public string Type { get; set; }
+    public int Number { get; set; }
+    public string NotifiedBody { get; set; }
+    public DateTime IssueDate { get; set; }
+    public DateTime ExpiryDate { get; set; }
+}
