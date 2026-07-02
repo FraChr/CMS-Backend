d3cfc6dd6c22bb18fab5434442cda7f807f11ac5
FraChr <empynia@gmail.com>
2026-07-02 17:31:46 +0200

    logic for crud operation on certificates entity

--- Api/Services/CertificateService.cs
+++ Api/Services/CertificateService.cs
@@ -1,6 +1,74 @@
-﻿namespace Api.Services;
+﻿using Api.Dtos;
+using Api.Interfaces;
+using Api.Model;
+using Microsoft.EntityFrameworkCore;
 
-public class CertificateService
+namespace Api.Services;
+
+public class CertificateService : ICertificateService
 {
+    private readonly Context _context;
+    private readonly IFileStorageService _fileStorageService;
     
+    public CertificateService(Context context, IFileStorageService fileStorageService)
+    {
+        _context = context;
+        _fileStorageService = fileStorageService;
+    }
+
+    public async Task<List<Certificate>> GetCertificatesAsync(CertificateFilter filter)
+    {
+        var query = _context.Certificates.AsQueryable();
+
+        if (filter.Number.HasValue)
+        {
+            query = query.Where(x => x.Number == filter.Number);
+        }
+
+        if (!string.IsNullOrWhiteSpace(filter.Type))
+        {
+            query = query.Where(x => x.Type == filter.Type);
+        }
+        
+        return await query.ToListAsync();
+    }
+    
+    
+    public async Task<Certificate> UploadCertificate(Certificate certificate, IFormFile file)
+    {
+        try
+        {
+            var filePath = await _fileStorageService.SaveFileAsync(file);
+
+            certificate.FilePath = filePath;
+            
+            _context.Certificates.Add(certificate);
+            await _context.SaveChangesAsync();
+
+            return certificate;
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Failed to create certificate", e);
+        }
+    }
+
+    public async Task<CertificateFileResult> GetCertificateFileAsync(int id)
+    {
+        var certificate = await _context.Certificates.FindAsync(id);
+
+        if (certificate == null || string.IsNullOrWhiteSpace(certificate.FilePath))
+        {
+            return null;
+        }
+        
+        var fileStream = await _fileStorageService.GetFileAsync(certificate.FilePath);
+
+        return new CertificateFileResult
+        {
+            Stream = fileStream,
+            FileName = certificate.FilePath,
+            ContentType = "application/pdf"
+        };
+    }
 }
