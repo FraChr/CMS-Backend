f5f8331f74f98a752407209b8d175db52cb84b2c
FraChr <empynia@gmail.com>
2026-07-02 18:46:38 +0200

    updated the return value of getCertificatesAsync

--- Api/Interfaces/ICertificateService.cs
+++ Api/Interfaces/ICertificateService.cs
@@ -5,7 +5,7 @@ namespace Api.Interfaces;
 
 public interface ICertificateService
 {
-    public Task<List<Certificate>> GetCertificatesAsync(CertificateFilter filter);
+    public Task<List<CertificateView>> GetCertificatesAsync(CertificateFilter filter);
     public Task<Certificate> UploadCertificate(Certificate certificate, IFormFile file);
     public Task<CertificateFileResult> GetCertificateFileAsync(int id);
 }
