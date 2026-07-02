0ee1a68192974d74419c983f7047445ba73e0930
FraChr <empynia@gmail.com>
2026-07-02 17:13:17 +0200

    DTOs for post and get of certificates

--- Api/Dtos/CertificateFileResult.cs
+++ Api/Dtos/CertificateFileResult.cs
@@ -2,5 +2,7 @@
 
 public class CertificateFileResult
 {
-    
+    public Stream? Stream { get; set; } = null;
+    public string FileName { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
 }
--- Api/Dtos/CreateCertificateRequest.cs
+++ Api/Dtos/CreateCertificateRequest.cs
@@ -2,5 +2,9 @@
 
 public class CreateCertificateRequest
 {
-    
+    public string Type { get; set; } = "";
+    public int Number { get; set; }
+    public string NotifiedBody { get; set; } = "";
+    public DateTime IssueDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
 }
